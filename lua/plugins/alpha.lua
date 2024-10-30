00b2ebb0bd1170919e7941eee5abedff09ffc12c
Daryl Ou <daryl.ou@appfolio.com>
2024-10-30 16:03:58 -0700

    alpha dash configuration

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,46 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+    local section = dashboard.section
+    local fn = vim.fn
+
+    section.header.val = {
+      [[   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ]],
+      [[    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ]],
+      [[          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ]],
+      [[           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ]],
+      [[          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ]],
+      [[   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ]],
+      [[  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ]],
+      [[ ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ]],
+      [[ ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ]],
+      [[      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ]],
+      [[       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ]],
+      [[                                   ]],
+      [[               darylou            ]],
+    }
+
+    section.buttons.val = {
+      dashboard.button( "e", "    New file" , ":ene <BAR> startinsert <CR>"),
+      dashboard.button( "f", "    Find file", ":Telescope find_files<CR>"),
+      dashboard.button( "r", "    Recent"   , ":Telescope oldfiles<CR>"),
+      dashboard.button( "s", "    Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button( "q", "    Quit NVIM", ":qa<CR>"),
+    }
+
+
+    local headerPadding = fn.max({ 2, fn.floor(fn.winheight(0) * 0.3) })
+
+    alpha.setup({
+      layout = {
+        { type = "padding", val = headerPadding },
+        section.header,
+        { type = "padding", val = 2 },
+        section.buttons,
+      },
+    })
+  end,
+}
