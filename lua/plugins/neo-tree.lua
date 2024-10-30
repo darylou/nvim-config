bf346aa9af445f6e238eba7e49d4928540f708ad
Daryl Ou <daryl.ou@appfolio.com>
2024-10-30 10:13:01 -0700

    further lsp configuration

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,5 +9,16 @@ return {
   config = function()
     vim.keymap.set('n', '<leader>no', ':Neotree filesystem reveal left<CR>')
     vim.keymap.set('n', '<leader>nc', ':Neotree close<CR>')
+
+    require('neo-tree').setup({
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        }
+      }
+    })
   end
 }
