cad89b0de52a10452c11d7a30d7b1074dddb9cb8
Daryl Ou <darylou@ucsb.edu>
2024-10-30 18:06:51 -0700

    formatting

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -37,13 +37,12 @@ return {
     }
 
     section.buttons.val = {
-      dashboard.button( "e", "    New file" , ":ene <BAR> startinsert <CR>"),
-      dashboard.button( "f", "    Find file", ":Telescope find_files<CR>"),
-      dashboard.button( "r", "    Recent"   , ":Telescope oldfiles<CR>"),
-      dashboard.button( "s", "    Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-      dashboard.button( "q", "    Quit NVIM", ":qa<CR>"),
+      dashboard.button("e", "    New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "    Find file", ":Telescope find_files<CR>"),
+      dashboard.button("r", "    Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("s", "    Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button("q", "    Quit NVIM", ":qa<CR>"),
     }
-
 
     local headerPadding = fn.max({ 2, fn.floor(fn.winheight(0) * 0.2) })
 
